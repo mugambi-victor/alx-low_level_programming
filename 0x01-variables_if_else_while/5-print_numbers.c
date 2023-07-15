2bb5af927e5e0616a103cf0566408f89af97020c
mugambi-victor <victormmugambi@gmail.com>
2023-07-14 23:26:51 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main- Entry point
+ * Description: A program that prints all base 10 numbers
+ * Return: 0 always
+ **/
+int main(void)
+{
+	for (int i = 0; i < 10; i++)
+	{
+		printf(i);
+	}
+	putchar('\n');
+	return (0);
+}
