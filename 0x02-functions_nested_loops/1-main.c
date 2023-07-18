1f1704482c9d2a2811f51417a0abf0f69db9af41
mugambi-victor <victormmugambi@gmail.com>
2023-07-18 01:59:55 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-main.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * main- Entry point
+ * Description: a program that prints alphabet in lowercase
+ * Return: Always 0;
+ **/
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
