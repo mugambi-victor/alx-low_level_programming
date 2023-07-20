ea5425cf5c5be2f2fd7977c2b6fc9d762b137d02
mugambi-victor <victormmugambi@gmail.com>
2023-07-20 03:43:37 -0700

    6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,26 +1,17 @@
 #include "main.h"
 
 /**
- * print_diagonal - Draws a diagonal line using the \ character.
- * @n: The number of \ characters to be printed.
+ * print_line - Draws a straight line using the character _.
+ * @n: The number of _ characters to be printed.
  */
-void print_diagonal(int n)
+void print_line(int n)
 {
-	int len, space;
+	int len;
 
 	if (n > 0)
 	{
 		for (len = 0; len < n; len++)
-		{
-			for (space = 0; space < len; space++)
-				_putchar(' ');
-			_putchar('\\');
-
-			if (len == n - 1)
-				continue;
-
-			_putchar('\n');
-		}
+			_putchar('_');
 	}
 
 	_putchar('\n');
