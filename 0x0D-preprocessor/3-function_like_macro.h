c05b3e335a108ee181b126a3c582368931444448
mugambi-victor <victormmugambi@gmail.com>
2023-08-15 00:34:03 -0700

    fixx

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -4,8 +4,8 @@
 /*
  * Desc: Header file that defines a function-like macro ABS(x)
  *       that computes the absolute value of a number x.
- */i
+ */
 
-#define ABS(x) ((x) < 0 ? (-x) : (x))
+#define ABS(x) ((x) < 0 ? -(x) : (x))
 
 #endif
