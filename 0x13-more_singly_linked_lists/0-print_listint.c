3852fccda7fa96a7eff3dc3c4fb6a7bae978bbf7
mugambi-victor <victormmugambi@gmail.com>
2023-08-28 08:49:49 -0700

    betty fix

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -3,25 +3,24 @@
 
 /**
  * print_listint - Prints the elements of a linked list containing integers.
- * @head: Pointer to the head node of the linked list.
+ * @h: Pointer to the head node of the linked list.
  * Return: The number of nodes in the linked list.
  */
 size_t print_listint(const listint_t *h)
 {
-    size_t count = 0; 
+	size_t count = 0;
 
-    while (h != NULL)
-    {
-        printf("%d\n", h->n);
-        /*
-        *Move to the next node
-        */
-        h = h->next;
-        /*
-        *Increment the counter
-        */
-        count++;
-    }
-
-    return count;
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		/**
+		 * Move to the next node
+		 */
+		 h = h->next;
+		 /**
+		  * Increment the counter
+		  */
+		 count++;
+	}
+	return (count);
 }
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -2,13 +2,22 @@
 #define LISTS_H
 #include <stddef.h>
 
-/*
-*LISTS_H file contains all function prototypes for functions used in more linked lists project.
-*/
+/**
+ * Description: lists.h file contains all function prototypes
+ * for functions used in more linked lists project.
+ */
+
+/**
+ * listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ */
 typedef struct listint_s
 {
-    int n;
-    struct listint_s *next;
+	int n;
+	struct listint_s *next;
 } listint_t;
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
