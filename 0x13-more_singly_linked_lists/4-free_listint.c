95fd28494b87064bc83eb62d88a94ab801e686b0
mugambi-victor <victormmugambi@gmail.com>
2023-08-28 09:37:41 -0700

    fixx

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -2,20 +2,21 @@
 #include "lists.h"
 
 /**
- * free_listint - Frees all nodes of a linked list containing integer values.
+ * free_listint - Frees all nodes
+ * of a linked list containing integer values.
  * @head: Pointer to the head node of the linked list.
  */
 void free_listint(listint_t *head)
 {
-    listint_t *current;
+	listint_t *current;
 
-    while (head != NULL)
-    {
-        current = head;
-        head = head->next;
-	/**
-	 * free the current node
-	 */
-        free(current);
-    }
+	while (head != NULL)
+	{
+		current = head;
+		head = head->next;
+		/**
+		 * free the current node
+		 */
+		free(current);
+	}
 }
