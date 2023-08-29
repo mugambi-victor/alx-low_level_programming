4239bc34e97d3b9867ef5713b6be2154020ff1d9
mugambi-victor <victormmugambi@gmail.com>
2023-08-29 00:17:06 -0700

    reverse string

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+
+/**
+ * reverse_listint - Reverses the order of
+ * elements in a linked list.
+ * @head: A pointer to the head node of the linked list.
+ */
+void reverse_listint(listint_t **head)
+{
+	listint_t *prev = NULL;
+	listint_t *current = *head;
+	listint_t *next;
+
+	while (current != NULL)
+	{
+		/**
+		 * Store the next node and reverse the link
+		 */
+		next = current->next;
+		current->next = prev;
+		/**
+		 * Move prev and current forward
+		 */
+		prev = current;
+		current = next;
+	}
+
+	/**
+	 * Update the head to point tO
+	 * the new first node (previously last node)
+	 */
+	*head = prev;
+}
