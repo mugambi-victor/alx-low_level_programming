a6cac307bab8a3c4752f62d334c7b1be776bc049
mugambi-victor <victormmugambi@gmail.com>
2023-09-05 12:15:32 +0300

    copy fix

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,137 +1,93 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <fcntl.h>
+#include <unistd.h>
 #include <errno.h>
 #include <sys/stat.h>
 
 #define BUFFER_SIZE 1024
 
-int open_source_file(const char *filename);
-int open_destination_file(const char *filename);
-ssize_t read_and_write(int src_fd, int dest_fd);
+void close_file(int fd);
+void handle_error(const char *message, const char *file, int exit_code);
+
 /**
- * main - Entry point of the program.
+ * main - Copies the contents of a file to another file.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
  *
- * @argc: The number of command-line arguments.
- * @argv: An array of command-line arguments.
+ * Return: 0 on success.
  *
- * Return: 0 on success, or an error code on failure.
+ * Description: If the argument count is incorrect - exit code 97.
+ *              If file_from does not exist or cannot be read - exit code 98.
+ *              If file_to cannot be created or written to - exit code 99.
+ *              If file_to or file_from cannot be closed - exit code 100.
  */
 int main(int argc, char *argv[])
 {
-	int src_fd, dest_fd;
-	ssize_t result;
+	int from, to, r, w;
+	char buffer[BUFFER_SIZE];
 
 	if (argc != 3)
 	{
-		dprintf(2, "Usage: %s file_from file_to\n", argv[0]);
-		return (97);
-	}
-	src_fd = open_source_file(argv[1]);
-
-	if (src_fd == -1)
-		return (98);
-
-	dest_fd = open_destination_file(argv[2]);
-
-	if (dest_fd == -1)
-	{
-		close(src_fd);
-		return (99);
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		return (97); /* Exit with code 97 for incorrect argument count. */
 	}
 
-	result = read_and_write(src_fd, dest_fd);
-
-	if (close(src_fd) == -1 || close(dest_fd) == -1)
+	from = open(argv[1], O_RDONLY);
+	if (from == -1)
 	{
-		dprintf(2, "Error: Can't close file descriptors\n");
-		return (100);
+		handle_error("Can't open source file", argv[1], 98);
 	}
 
-	return (result == -1 ? 98 : 0);
-}
-/**
- * open_source_file - Open the source file for reading.
- *
- * @filename: The path to the source file to be opened.
- *
- * Return: The file descriptor on success, or -1 on failure.
- *         If the file cannot be opened, an error message is printed.
- */
-int open_source_file(const char *filename)
-{
-	int fd = open(filename, O_RDONLY);
-
-	if (fd == -1)
-		dprintf(2, "Error: Can't read from file %s\n", filename);
-	return (fd);
-}
-/**
- * open_destination_file - Open the destination file for writing.
- *
- * @filename: The destination file path.
- *
- * Return: The file descriptor on success, or -1 on failure.
- */
-int open_destination_file(const char *filename)
-{
-	mode_t file_permissions = S_IRUSR | S_IWUSR | S_IRGRP;
-	int fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, file_permissions);
-
-	if (fd == -1)
+	to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (to == -1)
 	{
-		dprintf(2, "Error: Can't write to %s\n", filename);
+		handle_error("Can't open destination file", argv[2], 99);
 	}
-	else
-	{
-		/**
-		 * check if the file was just created
-		 */
 
-		if (errno == EEXIST)
+	do
+	{
+		r = read(from, buffer, BUFFER_SIZE);
+		if (r == -1)
 		{
-			/**
-			 * If the file already existed, don't change its permissions
-			 */
-			file_permissions = S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH;
-			chmod(filename, file_permissions);
+			handle_error("Can't read from file", argv[1], 98);
 		}
-	}
 
-	return (fd);
-}
-/**
- * read_and_write - Read from the source file descriptor and write to the
- *                  destination file descriptor.
- *
- * @src_fd: The source file descriptor.
- * @dest_fd: The destination file descriptor.
- *
- * Return: The total number of bytes written to the destination file descriptor
- *         on success, or -1 on failure.
- */
-ssize_t read_and_write(int src_fd, int dest_fd)
-{
-	char buffer[BUFFER_SIZE];
-	ssize_t bytes_read, bytes_written, total_bytes_written = 0;
-
-	while ((bytes_read = read(src_fd, buffer, BUFFER_SIZE)) > 0)
-	{
-		bytes_written = write(dest_fd, buffer, bytes_read);
-		if (bytes_written == -1)
+		w = write(to, buffer, r);
+		if (w == -1)
 		{
-			dprintf(2, "Error: Can't write to destination file\n");
-			return (-1);
+			handle_error("Can't write to file", argv[2], 99);
 		}
-		total_bytes_written += bytes_written;
-	}
 
-	if (bytes_read == -1)
+	} while (r > 0);
+
+	close_file(from);
+	close_file(to);
+
+	return (0);
+}
+
+/**
+ * close_file - Closes a file descriptor.
+ * @fd: The file descriptor to be closed.
+ */
+void close_file(int fd)
+{
+	if (close(fd) == -1)
 	{
-		dprintf(2, "Error: Can't read from source file\n");
-		return (-1);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		exit(100); /* Exit with code 100 if closing the file descriptor fails. */
 	}
+}
 
-	return (total_bytes_written);
+/**
+ * handle_error - Handles and reports an error.
+ * @message: The error message to be displayed.
+ * @file: The name of the file associated with the error.
+ * @exit_code: The exit code to use when terminating the program.
+ */
+void handle_error(const char *message, const char *file, int exit_code)
+{
+	dprintf(STDERR_FILENO, "Error: %s %s\n", message, file);
+	exit(exit_code);
 }
