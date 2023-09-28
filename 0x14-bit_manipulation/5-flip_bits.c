349dd83cfa5912fcdeb072f99bf9f5b3b98b7180
mugambi-victor <victormmugambi@gmail.com>
2023-09-28 13:19:01 +0300

    bit manipulation

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -10,10 +10,12 @@ unsigned int flip_bits(unsigned int n, unsigned int m)
 	unsigned int xor_result = n ^ m;
 	unsigned int count = 0;
 
-	while (xor_result != 0)
+	
+	while (xor_result > 0)
 	{
-		count += xor_result & 1;
+		count += (xor_result & 1);
 		xor_result >>= 1;
 	}
+
 	return (count);
 }
