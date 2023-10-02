e321c8b204e2f058e69f5dbf6dac4af2c04f3ad9
mugambi-victor <victormmugambi@gmail.com>
2023-10-02 13:07:29 +0300

    task 3

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+
+/**
+ * append_text_to_file - Appends text to the end of a file.
+ * @filename: The name of the file.
+ * @text_content: The NULL-terminated string to add at the end of the file.
+ *
+ * Return: 1 on success, -1 on failure.
+ *         -1 if the file can't be opened for appending,
+ *         -1 if write fails.
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int file_descriptor;
+
+	if (filename == NULL)
+		return (-1);
+	file_descriptor = open(filename, O_WRONLY | O_APPEND);
+	if (file_descriptor == -1)
+		return (-1);
+	if (text_content != NULL)
+	{
+		ssize_t write_result;
+
+		write_result = write(file_descriptor, text_content, strlen(text_content));
+		if (write_result == -1)
+		{
+			close(file_descriptor);
+			return (-1);
+		}
+	}
+	close(file_descriptor);
+	return (1);
+}
+
