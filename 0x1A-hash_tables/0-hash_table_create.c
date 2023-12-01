138f500f545f11fddf4887b83dc2afbe3b11a384
mugambi-victor <victormmugambi@gmail.com>
2023-12-01 10:23:44 +0300

    task 2

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -12,12 +12,12 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *hash_table;
 	unsigned long int i;
 
-	/* Allocate memory for the hash table structure */
+	/* Allocating memory for the hash table structure */
 	hash_table = malloc(sizeof(hash_table_t));
 	if (hash_table == NULL)
 		return (NULL);
 
-	/* Allocate memory for the array of hash_node_t pointers */
+	/* Allocating memory for the array of hash_node_t pointers */
 	hash_table->array = malloc(sizeof(hash_node_t *) * size);
 	if (hash_table->array == NULL)
 	{
@@ -29,7 +29,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	for (i = 0; i < size; i++)
 		hash_table->array[i] = NULL;
 
-	/* Set the size of the hash table */
+	/* Setting the size of the hash table */
 	hash_table->size = size;
 
 	return (hash_table);
